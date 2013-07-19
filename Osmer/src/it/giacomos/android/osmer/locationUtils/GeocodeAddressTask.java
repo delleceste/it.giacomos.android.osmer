1e44a703582a71049741629efb85b2b37bc55d92
Giacomo <delleceste@gmail.com>
2013-07-19 17:09:16 +0200

    Added check for null in doInBackground in GeocodeAddressTask.java.
    Problem signaled through play store

--- Osmer/src/it/giacomos/android/osmer/locationUtils/GeocodeAddressTask.java
+++ Osmer/src/it/giacomos/android/osmer/locationUtils/GeocodeAddressTask.java
@@ -47,7 +47,7 @@ public class GeocodeAddressTask extends AsyncTask<Location, Integer, LocationInf
 					locationInfo.provider = location[0].getProvider();
 					locationInfo.accuracy = location[0].getAccuracy();
 					List<Address> listAddresses = geocoder.getFromLocation(location[0].getLatitude(), location[0].getLongitude(), 1);
-					if(listAddresses != null)
+					if(listAddresses != null && listAddresses.get(0) != null)
 					{
 						locationInfo.address = "";
 						locationInfo.locality = "";
