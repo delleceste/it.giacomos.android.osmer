be7bfc912ed9a33fd3d16987ef395333dcb642c8
Giacomo <delleceste@gmail.com>
2013-10-24 16:29:15 +0200

    The popup at startup now announces the great news of the pro version: animation, area/strip detailed forecast, trend for the third day...
    1.2.5

--- Osmer/src/it/giacomos/android/osmer/preferences/Settings.java
+++ Osmer/src/it/giacomos/android/osmer/preferences/Settings.java
@@ -129,14 +129,14 @@ public class Settings
 	
 	public boolean isUpgradeDialogEnabled() 
 	{
-		boolean res = mSharedPreferences.getBoolean("UPGRADE_TO_PRO_DIALOG_ENABLED_1_2_4", true);
+		boolean res = mSharedPreferences.getBoolean("UPGRADE_TO_PRO_DIALOG_ENABLED_1_2_5", true);
 		return res;
 	}
 	
 	public void setUpgradeDialogEnabled(boolean en) 
 	{
 		SharedPreferences.Editor e = mSharedPreferences.edit();
-		e.putBoolean("UPGRADE_TO_PRO_DIALOG_ENABLED_1_2_4", en);
+		e.putBoolean("UPGRADE_TO_PRO_DIALOG_ENABLED_1_2_5", en);
 		e.commit();
 	}
 	
