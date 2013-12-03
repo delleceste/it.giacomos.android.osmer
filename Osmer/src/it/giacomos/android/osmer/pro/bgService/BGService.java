e1577e8ca619abd83fae4816bf009df6487e7be7
Giacomo <delleceste@gmail.com>
2013-12-03 18:33:55 +0100

    Towards service....

---
+++ Osmer/src/it/giacomos/android/osmer/pro/bgService/BGService.java
@@ -0,0 +1,16 @@
+package it.giacomos.android.osmer.pro.bgService;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BGService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) 
+	{
+		
+		return null;
+	}
+
+}
