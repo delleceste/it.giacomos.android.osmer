9fb93084b7331bbc286bc1edb90c9bf0bfeff620
Giacomo <delleceste@gmail.com>
2016-06-01 11:58:39 +0200

    Fixed timestamps: now they are in Localtime

--- Osmer/src/it/giacomos/android/osmer/widgets/map/animation/AnimationData.java
+++ Osmer/src/it/giacomos/android/osmer/widgets/map/animation/AnimationData.java
@@ -1,6 +1,11 @@
 package it.giacomos.android.osmer.widgets.map.animation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /* since version 2.5, the php script "get_radar_files.php" prepares a timestamp 
  * string which is ready to use, without the need to parse it.
@@ -9,21 +14,24 @@ public class AnimationData {
 
 	public AnimationData(String tim, String fName)
 	{
-		int lastIndexOfColon = tim.lastIndexOf(':');
-		int indexOfFirstHyphen = tim.indexOf('-');
-		String yearstr = "";
-
-		if(lastIndexOfColon > -1 && indexOfFirstHyphen > -1)
-		{
-			yearstr = tim.substring(0, indexOfFirstHyphen);
-			time = tim.substring(0, lastIndexOfColon);
-			/* remove year if the same as current year */
-			int year = Calendar.getInstance().get(Calendar.YEAR);
-			if(year == Integer.parseInt(yearstr))
-				time = time.substring(indexOfFirstHyphen + 1);
-		}
-		else
+		/* tim arrives in the format DAY/MONTH */
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		String format = "yyyy/dd/MM HH:mm";
+		String input = String.valueOf(year) + "/" + tim;
+		SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
+		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+		
+		try {
+			Date date = sdf.parse(input);
+			String outFormat = "dd.MM.yyyy HH:mm";
+			sdf = new SimpleDateFormat(outFormat);
+			sdf.setTimeZone(TimeZone.getDefault());
+			time = sdf.format(date);
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
 			time = tim;
+		}
 		
 		fileName = fName;
 	}
